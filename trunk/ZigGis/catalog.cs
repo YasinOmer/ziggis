19a411b2e8e38cacc23ddbdc44da62f02d5dd8a9
dollins.bill <dollins.bill@88a0873e-d223-0410-92d6-273f1e815a3c>
2007-03-06 11:47:19 +0000

    Added wizard class to fire dialog box.

--- trunk/ZigGis/catalog.cs
+++ trunk/ZigGis/catalog.cs
@@ -1,13 +1,21 @@
 using System;
 using System.Data;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
 using Npgsql;
+using System.Drawing;
 using System.Text;
+using ZigGis.PostGis;
 using ZigGis.Utilities;
 using ESRI.ArcGIS.Catalog;
+using ESRI.ArcGIS.esriSystem;
 
-namespace ZigGis.PostGis
+namespace ZigGis.PostGis.Catalog
 {
-    public class GxPostGisDatabaseFolder :
+    [ClassInterface(ClassInterfaceType.None)]
+    [ProgId("ComInteropClass")]
+    [Guid("AFFC20DB-F7C7-4cc5-95A2-E6914EBD94A1")]
+    public class GxOgcDatabaseFolder :
         IGxCachedObjects,
         IGxDataElement,
         IGxDataElementHelper,
@@ -19,6 +27,58 @@ namespace ZigGis.PostGis
         IGxPasteTarget,
         IGxRemoteContainer
     {
+        private IGxObject m_parentObject = null;
+        private IGxCatalog m_parentCatalog = null;
+        private IGxObjectArray m_children = null;
+        private bool m_childrenLoaded = false;
+        private Bitmap m_bitmapLrg;
+        private IntPtr m_hBitmapLrg;
+        private Bitmap m_bitmapSm;
+        private IntPtr m_hBitmapSm;
+
+        public GxOgcDatabaseFolder()
+        {
+            m_children = new GxObjectArrayClass();
+            m_bitmapLrg = new Bitmap(GetType().Assembly.GetManifestResourceStream("Pulp.ArcGis.ZigGis.images.ogc_lrg.bmp"));
+            m_bitmapLrg.MakeTransparent(m_bitmapLrg.GetPixel(1, 1));
+            m_hBitmapLrg = m_bitmapLrg.GetHbitmap();
+            m_bitmapSm = new Bitmap(GetType().Assembly.GetManifestResourceStream("Pulp.ArcGis.ZigGis.images.ogc_sm.bmp"));
+            m_bitmapSm.MakeTransparent(m_bitmapSm.GetPixel(1, 1));
+            m_hBitmapSm = m_bitmapSm.GetHbitmap();
+        }
+
+        ~GxOgcDatabaseFolder()
+        {
+            DeleteObject(m_hBitmapLrg);
+            DeleteObject(m_hBitmapSm);
+        }
+
+        //HKEY_CLASSES_ROOT\Component Categories\{6E208C9A-DBD3-11D2-9F2F-00C04F6BC69E}
+        #region Windows API
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        static extern bool DeleteObject(IntPtr hObject);
+        #endregion
+
+        #region COM Registration
+        [ComRegisterFunction()]
+        static void RegistrationFunction(Type t)
+        {
+            string key = @"\CLSID\{" + t.GUID.ToString() + @"}\Implemented Categories";
+            RegistryKey regKey = Registry.ClassesRoot.OpenSubKey(key, true);
+            if (regKey != null)
+                regKey.CreateSubKey("{6E208C9A-DBD3-11D2-9F2F-00C04F6BC69E}");
+        }
+
+        [ComUnregisterFunction()]
+        static void UnregistrationFunction(Type t)
+        {
+            string key = @"\CLSID\{" + t.GUID.ToString() + @"}\Implemented Categories";
+            RegistryKey regKey = Registry.ClassesRoot.OpenSubKey(key, true);
+            if (regKey != null)
+                regKey.DeleteSubKey("{6E208C9A-DBD3-11D2-9F2F-00C04F6BC69E}");
+        }
+        #endregion
+
         #region IGxCachedObjects Members
 
         public void LoadCachedObjects()
@@ -60,22 +120,29 @@ namespace ZigGis.PostGis
 
         public void Attach(IGxObject Parent, IGxCatalog pCatalog)
         {
-            throw new Exception("The method or operation is not implemented.");
+            m_parentCatalog = pCatalog;
+            m_parentObject = Parent;
+            //throw new Exception("The method or operation is not implemented.");
         }
 
         public string BaseName
         {
-            get { throw new Exception("The method or operation is not implemented."); }
+            get { return "OGC Database Connections"; }
         }
 
         public string Category
         {
-            get { throw new Exception("The method or operation is not implemented."); }
+            get { return "OGC Database Connections"; }
         }
 
         public ESRI.ArcGIS.esriSystem.UID ClassID
         {
-            get { throw new Exception("The method or operation is not implemented."); }
+            get 
+            {
+                IUID uid = new UIDClass();
+                uid.Value = "{AFFC20DB-F7C7-4cc5-95A2-E6914EBD94A1}";
+                return uid as UID;
+            }
         }
 
         public void Detach()
@@ -85,32 +152,35 @@ namespace ZigGis.PostGis
 
         public string FullName
         {
-            get { throw new Exception("The method or operation is not implemented."); }
+            get { return "OGC Database Connections"; }
         }
 
         public ESRI.ArcGIS.esriSystem.IName InternalObjectName
         {
-            get { throw new Exception("The method or operation is not implemented."); }
+            get { return null; }
         }
 
         public bool IsValid
         {
-            get { throw new Exception("The method or operation is not implemented."); }
+            get { return true; }
         }
 
         public string Name
         {
-            get { throw new Exception("The method or operation is not implemented."); }
+            get { return "OGC Database Connections"; }
         }
 
         public IGxObject Parent
         {
-            get { throw new Exception("The method or operation is not implemented."); }
+            get { return m_parentObject; }
         }
 
         public void Refresh()
         {
-            throw new Exception("The method or operation is not implemented.");
+            m_children.Empty();
+            m_childrenLoaded = false;
+            addWizards();
+            //loadChildren;
         }
 
         #endregion
@@ -124,12 +194,16 @@ namespace ZigGis.PostGis
 
         public bool AreChildrenViewable
         {
-            get { throw new Exception("The method or operation is not implemented."); }
+            get { return true; }
         }
 
         public IEnumGxObject Children
         {
-            get { throw new Exception("The method or operation is not implemented."); }
+            get 
+            {
+                addWizards();
+                return m_children as IEnumGxObject;
+            }
         }
 
         public void DeleteChild(IGxObject child)
@@ -139,7 +213,7 @@ namespace ZigGis.PostGis
 
         public bool HasChildren
         {
-            get { throw new Exception("The method or operation is not implemented."); }
+            get { return true; }
         }
 
         #endregion
@@ -206,32 +280,32 @@ namespace ZigGis.PostGis
 
         public ESRI.ArcGIS.esriSystem.UID ContextMenu
         {
-            get { throw new Exception("The method or operation is not implemented."); }
+            get { return null; }
         }
 
         public int LargeImage
         {
-            get { throw new Exception("The method or operation is not implemented."); }
+            get { return m_hBitmapLrg.ToInt32(); }
         }
 
         public int LargeSelectedImage
         {
-            get { throw new Exception("The method or operation is not implemented."); }
+            get { return m_hBitmapLrg.ToInt32(); }
         }
 
         public ESRI.ArcGIS.esriSystem.UID NewMenu
         {
-            get { throw new Exception("The method or operation is not implemented."); }
+            get { return null; }
         }
 
         public int SmallImage
         {
-            get { throw new Exception("The method or operation is not implemented."); }
+            get { return m_hBitmapSm.ToInt32(); ; }
         }
 
         public int SmallSelectedImage
         {
-            get { throw new Exception("The method or operation is not implemented."); }
+            get { return m_hBitmapSm.ToInt32(); }
         }
 
         #endregion
@@ -246,6 +320,172 @@ namespace ZigGis.PostGis
         public bool Paste(ESRI.ArcGIS.esriSystem.IEnumName names, ref bool moveOperation)
         {
             throw new Exception("The method or operation is not implemented.");
+        }
+
+        #endregion
+
+        #region private members
+
+        private void addWizards()
+        {
+            if (!m_childrenLoaded)
+            {
+                GxAddPostGisConnection pgWizard = new GxAddPostGisConnection();
+                m_children.Insert(0, (IGxObject)pgWizard);
+                m_childrenLoaded = true;
+            }
+        }
+        #endregion
+    }
+
+    [ClassInterface(ClassInterfaceType.None)]
+    [ProgId("ComInteropClass")]
+    [Guid("56BF7C24-FCD3-4f9b-BE3D-CA5C2794C498")]
+    public class GxAddPostGisConnection :
+            IGxBasicObject,
+            IGxObject,
+            IGxObjectUI,
+            IGxObjectWizard
+    {
+        private IGxObject m_parentObject = null;
+        private IGxCatalog m_parentCatalog = null;
+        private IGxObjectArray m_children = null;
+        private bool m_childrenLoaded = false;
+        private Bitmap m_bitmapLrg;
+        private IntPtr m_hBitmapLrg;
+        private Bitmap m_bitmapSm;
+        private IntPtr m_hBitmapSm;
+
+        #region Windows API
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        static extern bool DeleteObject(IntPtr hObject);
+        #endregion
+
+        #region Constructors/Destructors
+        public GxAddPostGisConnection()
+        {
+            m_bitmapLrg = new Bitmap(GetType().Assembly.GetManifestResourceStream("Pulp.ArcGis.ZigGis.images.pgsql_lrg.bmp"));
+            m_bitmapLrg.MakeTransparent(m_bitmapLrg.GetPixel(1, 1));
+            m_hBitmapLrg = m_bitmapLrg.GetHbitmap();
+            m_bitmapSm = new Bitmap(GetType().Assembly.GetManifestResourceStream("Pulp.ArcGis.ZigGis.images.pgsql_sm.bmp"));
+            m_bitmapSm.MakeTransparent(m_bitmapSm.GetPixel(1, 1));
+            m_hBitmapSm = m_bitmapSm.GetHbitmap();
+
+        }
+
+        ~GxAddPostGisConnection()
+        {
+            DeleteObject(m_hBitmapLrg);
+            DeleteObject(m_hBitmapSm);
+        }
+        #endregion
+
+        #region IGxObject Members
+
+        public void Attach(IGxObject Parent, IGxCatalog pCatalog)
+        {
+            m_parentCatalog = pCatalog;
+            m_parentObject = Parent;
+            //throw new Exception("The method or operation is not implemented.");
+        }
+
+        public string BaseName
+        {
+            get { return "Add New PostGIS Connection"; }
+        }
+
+        public string Category
+        {
+            get { return "Add New PostGIS Connection"; }
+        }
+
+        public UID ClassID
+        {
+            get
+            {
+                IUID uid = new UIDClass();
+                uid.Value = "{56BF7C24-FCD3-4f9b-BE3D-CA5C2794C498}";
+                return uid as UID;
+            }
+        }
+
+        public void Detach()
+        {
+            //throw new Exception("The method or operation is not implemented.");
+        }
+
+        public string FullName
+        {
+            get { return "Add New PostGIS Connection"; }
+        }
+
+        public IName InternalObjectName
+        {
+            get { return null; }
+        }
+
+        public bool IsValid
+        {
+            get { return true; }
+        }
+
+        public string Name
+        {
+            get { return "Add New PostGIS Connection"; }
+        }
+
+        public IGxObject Parent
+        {
+            get { throw new Exception("The method or operation is not implemented."); }
+        }
+
+        public void Refresh()
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        #endregion
+
+        #region IGxObjectUI Members
+
+        public UID ContextMenu
+        {
+            get { throw new Exception("The method or operation is not implemented."); }
+        }
+
+        public int LargeImage
+        {
+            get { return m_hBitmapLrg.ToInt32(); }
+        }
+
+        public int LargeSelectedImage
+        {
+            get { return m_hBitmapLrg.ToInt32(); }
+        }
+
+        public UID NewMenu
+        {
+            get { throw new Exception("The method or operation is not implemented."); }
+        }
+
+        public int SmallImage
+        {
+            get { return m_hBitmapSm.ToInt32(); }
+        }
+
+        public int SmallSelectedImage
+        {
+            get { return m_hBitmapSm.ToInt32(); }
+        }
+
+        #endregion
+
+        #region IGxObjectWizard Members
+
+        public void Invoke(int hParentWnd)
+        {
+            ZigGis.ArcGIS.ArcMapUI.PostGisConnectionForm frmPgConnect = new ZigGis.ArcGIS.ArcMapUI.PostGisConnectionForm(hParentWnd);
+            frmPgConnect.Show();
         }
 
         #endregion
